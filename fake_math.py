e76b11266be8271fefb4c84b993efeeb18daf997
NikEr2024 <n.erdyneev2000@gmail.com>
2024-07-17 00:38:19 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    a = first
+    b = second
+    if b != 0:
+        return (a / b)
+    elif b ==0:
+        return ('ОШИБКА')
---
+++ true_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    d = first
+    c = second
+    if c != 0:
+        return (d / c)
+    elif c ==0:
+        return ("int")
